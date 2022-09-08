90cfb1a06bac9269625223439ac3a058270cc0d9
Kingsley Ibrahim <kingsleynkau@gmail.com>
2022-09-08 04:30:56 -0700

    done

--- parser.c
+++ parser.c
@@ -1,6 +1,4 @@
-#include "shell.h"
-
-/**                                                                    
+/** 
  * is_cmd - determines if a file is an executable command
  * @info: the info struct
  * @path: path to the file
